1529862feb1228446f6536a61f9847feeccfc6d7
Mateusz Olczak <olc.mateusz@gmail.com>
2023-09-05 14:42:44 +0200

    6 kyu - Divisible Ints

---
+++ src/test/java/pl/olcmat/codewars/sixkyu/divisibleints/KataTest.java
@@ -0,0 +1,21 @@
+package pl.olcmat.codewars.sixkyu.divisibleints;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class KataTest {
+
+    private static void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+   
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests getCount");
+        testing(Kata.getCount(123),2);
+        testing(Kata.getCount(1230),5);
+        testing(Kata.getCount(1),0);
+        testing(Kata.getCount(1111111111),25);
+    }
+    
+}
